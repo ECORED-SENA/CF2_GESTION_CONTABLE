aa3207eff06bbb568ebdc2ef6ad5d9b171a6e131
wroaserrano <walter.roas@misena.edu.co>
2021-07-13 09:32:01 -0500

    Update global.js

--- src/config/global.js
+++ src/config/global.js
@@ -23,7 +23,7 @@ export default {
         nombreRuta: 'tema1',
         icono: 'far fa-file-alt',
         numero: '1',
-        titulo: 'Información Contable',
+        titulo: 'Información contable',
         desarrolloContenidos: true,
         subMenu: [
           {
